c2516b9b427ba95e422448d39c848dfcdc71719e
Ivan Ramos <iardoru@gmail.com>
2025-06-30 22:10:24 -0500

    remove windows paddings and commented some machine specific code

--- config/app/wezterm/wezterm.lua
+++ config/app/wezterm/wezterm.lua
@@ -2,16 +2,22 @@ local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
 -- Quake Mode
-config.unix_domains = {
-  {
-    name = 'quake',
-  }
-}
+-- config.unix_domains = {
+--   {
+--     name = 'quake',
+--   }
+-- }
 
 config.audible_bell = "Disabled"
-config.default_prog = { "zsh" }
-config.enable_wayland = false
-config.front_end = "WebGpu"
+-- config.default_prog = { "zsh" }
+-- config.enable_wayland = false
+-- config.front_end = "WebGpu"
 config.window_background_opacity = 1.0
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 0,
+  bottom = 0,
+}
 
 return config
