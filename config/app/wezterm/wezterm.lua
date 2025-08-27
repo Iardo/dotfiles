67a957795c84b3998ac22db12e817e19849d605e
Iván Ramos <iardoru@gmail.com>
2025-08-26 23:28:20 -0500

    update wezterm config

--- config/app/wezterm/wezterm.lua
+++ config/app/wezterm/wezterm.lua
@@ -9,21 +9,21 @@ local config = wezterm.config_builder()
 -- }
 
 config = {
-	audible_bell = "Disabled"
+	audible_bell = "Disabled",
 	-- default_prog = { "zsh" }
 	-- enable_wayland = false
 	-- front_end = "WebGpu"
 	visual_bell = {
-                fade_in_duration_ms = 0,
-  		fade_out_duration_ms = 0,
+    fade_in_duration_ms = 0,
+    fade_out_duration_ms = 0,
 	},
-	window_background_opacity = 1.0
+	window_background_opacity = 1.0,
 	window_padding = {
-  		left = '1cell',
-  		right = '1cell',
-  		top = 0,
+    left = '1cell',
+    right = '1cell',
+    top = 0,
 		bottom = 0,
-	},	
+	},
 }
 
 return config
